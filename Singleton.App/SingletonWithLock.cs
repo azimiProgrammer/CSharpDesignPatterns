e85bd9a697ce6271454f4beebf8722101cd221cf
azimi_programer <azimi_programer@yahoo.com>
2023-04-18 10:39:01 +0430

    add Singleton project

---
+++ Singleton.App/SingletonWithLock.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton.App
+{
+    public class SingletonWithLock
+    {
+        private readonly static object _myLock = new();
+
+        private static SingletonWithLock? _instance;
+        private SingletonWithLock() { }
+        public static SingletonWithLock Create()
+        {
+            lock (_myLock)
+            {
+                if (_instance == default)
+                {
+                    _instance = new SingletonWithLock();
+                }
+            }
+            return _instance;
+        }
+    }
+}
---
+++ Singleton.Test/SingletonTest.cs
@@ -0,0 +1,20 @@
+﻿using Singleton.App;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton.Test
+{
+    public class SingletonTest
+    {
+        [Fact]
+        public void Create_should_always_return_the_same_instance()
+        {
+            var first = SingletonWithLock.Create();
+            var second = SingletonWithLock.Create();
+            Assert.Same(first, second);
+        }
+    }
+}
